566e6c74b85e04cbadf2817842349c4cc0bf43d9
Bozhidar Ivanov <77826575+b0zh1d4r@users.noreply.github.com>
2024-11-21 21:32:53 +0200

    Login Form changes
    I also added a maximum count directive for the login password form.

--- project-app/src/app/user/login/login.component.ts
+++ project-app/src/app/user/login/login.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { MaxCountDirective } from '../../directives/max-count.directive';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, MaxCountDirective],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('loginForm') form: NgForm | undefined;
 
+  maxCountNumber = 4;
+
+  formSubmitHandler() {
+    if (this.form?.invalid) {
+      console.log('This form is invalid!');
+      return;
+    }
+
+    console.log(this.form?.value);
+  }
 }
