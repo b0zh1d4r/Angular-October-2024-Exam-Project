6736337495ef5474e25820448a70aa42af846920
Bozhidar Ivanov <77826575+b0zh1d4r@users.noreply.github.com>
2024-12-01 16:34:20 +0200

    REST-API Course Controller and Service

---
+++ REST-API/src/controllers/courseController.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import courseService from "../services/courseService.js";
+
+const courseController = Router();
+
+courseController.get('/courses', async (req, res) => {
+    const courses = await courseService.getAll();
+    res.json(courses);
+});
+
+courseController.post('/create', async (req, res) => {
+    const course = await courseService.create(req.body);
+    
+    res.json(course);
+});
+
+export default courseController;
