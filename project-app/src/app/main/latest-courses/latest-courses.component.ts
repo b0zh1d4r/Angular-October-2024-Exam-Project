19a65329e8cc4deaa633f2faf0547e6b570ff02e
Bozhidar Ivanov <77826575+b0zh1d4r@users.noreply.github.com>
2024-12-06 13:36:23 +0200

    Added Latest Courses Functionality

--- project-app/src/app/main/latest-courses/latest-courses.component.ts
+++ project-app/src/app/main/latest-courses/latest-courses.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Course } from '../../types/course';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-latest-courses',
   standalone: true,
   imports: [],
   templateUrl: './latest-courses.component.html',
-  styleUrl: './latest-courses.component.css'
+  styleUrls: ['./latest-courses.component.css']
 })
 export class LatestCourseComponent {
+  latestCourses: Course[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getCourses().subscribe((courses) => {
+      // Assuming courses are already sorted by date (latest first).
+      for (let i = courses.length - 1; i >= courses.length - 3 && i >= 0; i--) {
+        if (courses[i] === undefined) {
+          return;
+        } else {
+          this.latestCourses.push(courses[i]);
+        }
+      }
+    });
+  }
 }
